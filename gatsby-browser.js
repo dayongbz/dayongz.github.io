7a164c006f9dd7fe24c8210c1c105ccb6179fc1b
Dayong Lee <50468628+dayongbz@users.noreply.github.com>
2020-12-22 21:45:13 +0900

    delete style post-list-item on hover

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -7,6 +7,6 @@ import "./src/normalize.css"
 import "./src/style.css"
 
 // Highlighting for code blocks
-import "prismjs/themes/prism.css"
+import "prismjs/themes/prism-tomorrow.css"
 
 import "fontsource-noto-sans-kr"
